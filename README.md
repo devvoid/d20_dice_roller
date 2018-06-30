58452896b602a7958ef90ffbd1f626ed3bbfb3cd
void <theprismaticvoid@gmail.com>
2018-06-29 23:12:32 -0500

    Ported to Serenity

--- README.md
+++ README.md
@@ -6,15 +6,14 @@ You can download a premade build of the bot from the [Releases page](https://git
 
 If you need to compile it for yourself, you can do it like so:
 
-1. Create a bot user, and get its key. Look up a bot-making tutorial if you have no idea how to do this.
-2. Clone/download this repository.
-3. In the bot's root directory (the same place as Cargo.toml), create a file named "bot.token"
-4. Put your bot token in bot.token.
-5. Build and run like you would any other Rust program.
+1. Install OpenSSL; Serenity requires it.
+2. Create a bot user, and get its key. Look up a bot-making tutorial if you have no idea how to do this.
+3. Clone/download this repository.
+4. In the bot's root directory (the same place as Cargo.toml), create a file named "bot.token"
+5. Put your bot token in bot.token.
+6. Build and run like you would any other Rust program.
 
-Requirements for Discord-RS can be found [here](https://github.com/SpaceManiac/discord-rs/).
-
-The bot is known to work on Linux Mint 18.2 and Windows 10, but everything here should work fine on older Windows versions, Mac OS X, or any other Linux distro.
+If you would prefer to not have your bot's token compiled into the executable, compile with the flag `--features external_token`. This will instead make the bot load the token from a `bot.token` file in the same folder as itself.
 
 If you don't want to deal with setup, you can use the version of the bot I run on my servers. Add it using [this](https://discordapp.com/oauth2/authorize?&client_id=440710365072982026&scope=bot&permissions=0) link. I would only recommend doing this if you can't get the bot to run yourself; this bot only runs while my computer is on, and because of that, it has a lot of downtime.
 
@@ -22,15 +21,17 @@ If you need any help, or have any questions, message me on Discord at Void#3651.
 
 ## Use
 
-There are three ways of using the bot:
+There are four ways of using the bot:
 
-`$r 20` - Rolls one 20-sided dice.
+`$r` - Rolls one twenty-sided die.
 
-`$r 2d20` - Rolls two 20-sided dice.
+`$r <number of faces>` - Rolls one die.
 
-`$r 2d20+5` - Rolls two 20-sided dice, with a modifier of 5.
+`$r <number of dice>d<number of faces>` - Rolls multiple dice.
 
-Obviously, the numbers can be substituted for any numbers you need.
+`$r <number of dice>d<number of faces>+<modifier>` - Rolls multiple dice, with a modifier. The modifier can be positive or negative.
+
+You can use either `$r` or `$roll`. The bot ignores spaces in the arguments.
 
 ## License
 
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,21 @@
 #![windows_subsystem = "windows"]
 
-extern crate discord;
+#[macro_use]
+extern crate serenity;
 extern crate rand;
 
-use discord::{Discord, State};
-use discord::model::{Event, Message};
+use serenity::client::Client;
+use serenity::prelude::EventHandler;
+use serenity::framework::standard::StandardFramework;
 
 use rand::Rng;
 
-#[derive(PartialEq)]
-enum ModifierState {
-	Plus,
-	Minus
-}
+struct Handler;
 
-#[cfg(feature = "externaltoken")]
-fn log_in() -> Discord {
+impl EventHandler for Handler {}
+
+#[cfg(feature = "external_token")]
+fn log_in() -> Client {
 	use std::fs::File;
 	use std::io::Read;
 
@@ -24,98 +24,131 @@ fn log_in() -> Discord {
 
     token_file.read_to_string(&mut token_string).expect("Failed to read token from bot.token.");
 
-	Discord::from_bot_token(&token_string[..]).expect("Failed to login with provided token.")
+	Client::new(&token_string[..], Handler).expect("Error creating client")
 }
 
-#[cfg(not(feature = "externaltoken"))]
-fn log_in() -> Discord {
-	Discord::from_bot_token(
-        include_str!("../bot.token")
-    ).expect("Failed to login with provided token.")
+#[cfg(not(feature = "external_token"))]
+fn log_in() -> Client {
+	Client::new(include_str!("../bot.token"), Handler).expect("Error creating client")
 }
 
 fn main() {
-    let discord = log_in();
+    // Login with a bot token from the environment
+    let mut client = log_in();
 
-    let (mut connection, ready) = discord.connect().expect("Failed to connect.");
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c.prefix("$"))
+		.cmd("roll", roll_command)
+		.cmd("r", roll_command));
 
-    let mut state = State::new(ready);
-
-    println!("Connected.");
-
-    loop {
-        let event = match connection.recv_event() {
-            Ok(event) => {
-                event
-            },
-
-            //TODO: Some error handling here would be beneficial.
-            _ => { continue }
-        };
-
-        state.update(&event);
-
-        match event {
-            Event::MessageCreate(message) => {
-                //Skip if the bot sends the message.
-                //Completely useless in this example, honestly.
-                if message.author.id == state.user().id {
-					continue
-                }
-
-				if message.content.len() < 1 {
-					continue;
-				}
-
-				if &message.content.as_str().chars().next() == Some("$") {
-                    let command_string : String = message.content[1 .. ].to_string();
-
-                    let mut command_data : Vec<&str> = command_string.split(' ').collect();
-
-
-                    match command_data[0] {
-                        "r" => {
-                            roll(&discord, &mut command_data, &message);
-                        }
-
-						"roll" => {
-							roll(&discord, &mut command_data, &message);
-						}
-
-                        _ => {
-							println!("Unknown command {}.", command_data[0]);
-							continue
-						}
-                    }
-                }
-            }
-
-            _ => {}
-        }
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
     }
 }
 
-fn roll(client: &Discord, args: &mut Vec<&str>, message: &Message) {
+command!(roll_command(_context, message) {
+	let response = roll(&message.content);
+	message.reply(&response[..]).expect("failed to send message");
+});
+
+fn roll(message: &String) -> String {
+	//Stop if message[3] isn't an ASCII string
+	if !message.is_ascii() {
+		return String::from("Failed to roll: Message is not ASCII");
+	}
+	
     //Get the thread's RNG.
     //You could probably move this to the generate_number function with no penalty,
     //but I'm not sure if repeatedly getting it from this function would incur overhead.
     let mut rng = rand::thread_rng();
+	
+	//This will be filled with three i32s. In order, these i32s are: the number of dice to roll, the number of faces, and the modifier.
+	let args: Vec<i32>;
+	
+	//Find the first space in the string. If there's not, that means the function was called with no arguments, meaning it should use the default roll of 1d20+0
+	//If it finds a space, convert to lowercase, strip all spaces, and go to the parse_args function to convert it into a Vec<i32>.
+	let first_space_location = message.find(' ').unwrap_or(0);
+	
+	if first_space_location == 0 {
+		args = vec!(1, 20, 0);
+	} else {
+		let unparsed_args = message.split_at(message.find(' ').unwrap_or(0)).1
+			.to_ascii_lowercase()
+			.replace(" ", "");
+			
+		args = parse_args(unparsed_args);
+	}
+	
+	//If either the number of dice, or the number of faces on the dice, are negative, bail out early.
+	if args[0] < 1 {
+		return String::from("Failed to roll: Number of dice is negative");
+	}
+	
+	if args[1] < 1 {
+		return String::from("Failed to roll: Number of faces is negative");
+	}
 
-    let mut roll_data: Vec<&str> = args[1].split("d").collect();
+    //Fill the vector with dice rolls.
+    let mut roll_results = String::new();
+	
+	roll_results.push('`');
+	
+	//The final total of numbers that will be displayed at the end of the function.
+	let mut total: i32 = 0;
 
-	let mut modifier_type = ModifierState::Plus;
+    for i in 0 .. args[0] {
+		let generated_number = generate_number(&mut rng, args[1]);
+		total += generated_number;
+		
+		let converted_number = generated_number.to_string();
+        roll_results.push_str(&converted_number[..]);
+		
+		if i < args[0] - 1 {
+			roll_results.push_str(" + ");
+		}
+    }
+
+    if args[2] != 0 {
+		roll_results.push(' ');
+        roll_results.push('(');
+
+        if args[2] > 0 {
+			roll_results.push('+');
+		} else {
+			roll_results.push('-');
+		}
+			
+		total += args[2];
+
+		let converted_args = args[2].to_string();
+        roll_results.push_str(&converted_args[..]);
+
+        roll_results.push(')');
+    }
+
+    roll_results.push('`');
+
+    //Combine roll_results and total together to form one message.
+    format!("{}. Total: {}", roll_results.as_str(), total)
+}
+
+fn parse_args(unparsed_args: String) -> Vec<i32> {
+	let args: &str = &unparsed_args[..];
+	
+    let mut roll_data: Vec<&str> = args.split("d").collect();
+	
+	let mut parsed_args: Vec<i32> = Vec::new();
 
     //This is extremely messy, but it was the only way I could think to get it to work.
     if roll_data.len() >= 2 {
+		//Attempt to split on the +. if that fails, split on the -.
         let mut modifier_data: Vec<&str> = roll_data[1].split("+").collect();
+		
         if modifier_data.len() < 2 {
             modifier_data = roll_data[1].split("-").collect();
-
-			if modifier_data.len() >= 2 {
-				modifier_type = ModifierState::Minus;
-			}
         }
-
+		
+		//If both splits fail to find anything, set the modifier to 0. Otherwise, set it to whatever was after the + or -.
         if modifier_data.len() < 2 {
             roll_data.push("0");
         } else {
@@ -123,98 +156,41 @@ fn roll(client: &Discord, args: &mut Vec<&str>, message: &Message) {
             roll_data[1] = modifier_data[0];
         }
     }
-
-    let number_of_dice: u32;
-    let dice_maximum: u32;
-    let modifier: i32;
-
+	
+	//Parse all the strings into their i32 counterparts.
+	//TODO: Replace all of this with unwrap_or, and find a way to make the bot say an error message if it's using a default variable.
     match roll_data.len() {
         1 => {
-            number_of_dice = 1;
-            dice_maximum = roll_data[0].parse::<u32>().unwrap();
-            modifier = 0;
+			parsed_args.push(1);
+			parsed_args.push(roll_data[0].parse::<i32>().unwrap());
+			parsed_args.push(0);
         },
 
         2 => {
-            number_of_dice = roll_data[0].parse::<u32>().unwrap();
-            dice_maximum = roll_data[1].parse::<u32>().unwrap();
-            modifier = 0;
+            parsed_args.push(roll_data[0].parse::<i32>().unwrap());
+			parsed_args.push(roll_data[1].parse::<i32>().unwrap());
+			parsed_args.push(0);
         }
 
         3 => {
-            number_of_dice = roll_data[0].parse::<u32>().unwrap();
-            dice_maximum = roll_data[1].parse::<u32>().unwrap();
-            modifier = roll_data[2].parse::<i32>().unwrap();
+            parsed_args.push(roll_data[0].parse::<i32>().unwrap());
+			parsed_args.push(roll_data[1].parse::<i32>().unwrap());
+			parsed_args.push(roll_data[2].parse::<i32>().unwrap());
         }
 
         _ => {
+			//This shouldn't be possible, but just in case, panic.
             panic!("roll_data.len() is outside of expected range");
         }
     }
-
-    //Fill the vector with dice rolls.
-    let mut roll_results = Vec::new();
-
-    for _i in 0 .. number_of_dice {
-        roll_results.push(generate_number(&mut rng, dice_maximum));
-    }
-
-    //total is the number that will be printed, showing all the dice added together.
-    let mut total: i32 = 0;
-
-    //dice_rolls is the visual display, showing the results of each individual die.
-    let mut dice_rolls = String::new();
-
-    dice_rolls.push('`');
-
-    let mut numbers_added = 0;
-
-    for i in roll_results.iter() {
-        let temporaryvalue = i.to_string();
-
-        dice_rolls.push_str(&temporaryvalue[..]);
-
-        total += *i as i32;
-        numbers_added += 1;
-
-        if numbers_added < number_of_dice {
-            dice_rolls.push_str(" + ");
-        }
-    }
-
-    if modifier != 0 {
-		dice_rolls.push(' ');
-        dice_rolls.push('(');
-
-        if modifier > 0 {
-			if modifier_type == ModifierState::Plus {
-				dice_rolls.push('+');
-				total += modifier;
-			} else {
-				dice_rolls.push('-');
-				total -= modifier;
-			}
-
-        }
-
-        dice_rolls.push_str(roll_data[2]);
-
-        dice_rolls.push(')');
-    }
-
-    dice_rolls.push('`');
-
-    //Combine dice_rolls and total together to form one message.
-    let response = format!("{}: {}. Total: {}", message.author.mention(), dice_rolls.as_str(), total);
-
-    //Send that message.
-    client.send_message(message.channel_id, response.as_str(), "", false).expect("Failed to send message");
+	
+	parsed_args
 }
 
-fn generate_number(rng: &mut rand::ThreadRng, max: u32) -> u32 {
-    //Between 1 and 4 because for some reason, it seems like rand won't generate the highest number given.
+fn generate_number(rng: &mut rand::ThreadRng, max: i32) -> i32 {
+    //max has to be increased by one, because rand never seems to generate the highest possible number given.
     //So if it's set to 1 and 3, it's always a 1 or 2 it generates.
-    let result: u32 = rng.gen_range::<u32>(1, max + 1);
+    let result: i32 = rng.gen_range::<i32>(1, max + 1);
 
     //For the reasons listed above, this should never happen, but if a future version of the rand crate changes the behavior,
     //this would let me know ahead of time.
